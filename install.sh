76bed3bfe04130c7f9e49d0f92956b1a054447c4
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-29 11:58:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
+#red='\033[0;31m'
+#green='\033[0;32m'
+#yellow='\033[0;33m'
+#plain='\033[0m'
 
 cur_dir=$(pwd)
 
