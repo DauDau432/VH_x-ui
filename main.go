532849665beceb4d8d0afe60f51e2ff9fb7f6f4a
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2022-02-26 10:11:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func runWebServer() {
 	case config.Error:
 		logger.InitLogger(logging.ERROR)
 	default:
-		log.Fatal("unknown log level:", config.GetLogLevel())
+		log.Fatal("cấp độ nhật ký không xác định:", config.GetLogLevel())
 	}
 
 	err := database.InitDB(config.GetDBPath())
@@ -58,7 +58,7 @@ func runWebServer() {
 		case syscall.SIGHUP:
 			err := server.Stop()
 			if err != nil {
-				logger.Warning("stop server err:", err)
+				logger.Warning("lỗi dừng máy chủ:", err)
 			}
 			server = web.NewServer()
 			global.SetWebServer(server)
@@ -84,9 +84,9 @@ func resetSetting() {
 	settingService := service.SettingService{}
 	err = settingService.ResetSettings()
 	if err != nil {
-		fmt.Println("reset setting failed:", err)
+		fmt.Println("đặt lại cài đặt không thành công:", err)
 	} else {
-		fmt.Println("reset setting success")
+		fmt.Println("đặt lại cài đặt thành công")
 	}
 }
 
@@ -102,18 +102,18 @@ func updateSetting(port int, username string, password string) {
 	if port > 0 {
 		err := settingService.SetPort(port)
 		if err != nil {
-			fmt.Println("set port failed:", err)
+			fmt.Println("đặt cổng không thành công:", err)
 		} else {
-			fmt.Printf("set port %v success", port)
+			fmt.Printf("đặt port %v thành công", port)
 		}
 	}
 	if username != "" || password != "" {
 		userService := service.UserService{}
 		err := userService.UpdateFirstUser(username, password)
 		if err != nil {
-			fmt.Println("set username and password failed:", err)
+			fmt.Println("đặt tên người dùng và mật khẩu không thành công:", err)
 		} else {
-			fmt.Println("set username and password success")
+			fmt.Println("đặt tên người dùng và mật khẩu thành công")
 		}
 	}
 }
@@ -125,32 +125,32 @@ func main() {
 	}
 
 	var showVersion bool
-	flag.BoolVar(&showVersion, "v", false, "show version")
+	flag.BoolVar(&showVersion, "v", false, "hiển thị phiên bản")
 
 	runCmd := flag.NewFlagSet("run", flag.ExitOnError)
 
 	v2uiCmd := flag.NewFlagSet("v2-ui", flag.ExitOnError)
 	var dbPath string
-	v2uiCmd.StringVar(&dbPath, "db", "/etc/v2-ui/v2-ui.db", "set v2-ui db file path")
+	v2uiCmd.StringVar(&dbPath, "db", "/etc/v2-ui/v2-ui.db", "đặt đường dẫn tệp v2-ui db")
 
 	settingCmd := flag.NewFlagSet("setting", flag.ExitOnError)
 	var port int
 	var username string
 	var password string
 	var reset bool
-	settingCmd.BoolVar(&reset, "reset", false, "reset all setting")
-	settingCmd.IntVar(&port, "port", 0, "set panel port")
-	settingCmd.StringVar(&username, "username", "", "set login username")
-	settingCmd.StringVar(&password, "password", "", "set login password")
+	settingCmd.BoolVar(&reset, "cài lại", false, "khôi phục tất cả cài đặt")
+	settingCmd.IntVar(&port, "port", 0, "đặt cổng bảng điều khiển")
+	settingCmd.StringVar(&username, "tên người dùng", "", "đặt tên người dùng đăng nhập")
+	settingCmd.StringVar(&password, "mật khẩu", "", "đặt mật khẩu đăng nhập")
 
 	oldUsage := flag.Usage
 	flag.Usage = func() {
 		oldUsage()
 		fmt.Println()
-		fmt.Println("Commands:")
-		fmt.Println("    run            run web panel")
-		fmt.Println("    v2-ui          migrate form v2-ui")
-		fmt.Println("    setting        set settings")
+		fmt.Println("Lệnh:")
+		fmt.Println("    run            chạy bảng điều khiển web")
+		fmt.Println("    v2-ui          di chuyển hình thức v2-ui")
+		fmt.Println("    setting        thiết lập cài đặt")
 	}
 
 	flag.Parse()
@@ -175,7 +175,7 @@ func main() {
 		}
 		err = v2ui.MigrateFromV2UI(dbPath)
 		if err != nil {
-			fmt.Println("migrate from v2-ui failed:", err)
+			fmt.Println("di chuyển từ v2-ui không thành công:", err)
 		}
 	case "setting":
 		err := settingCmd.Parse(os.Args[2:])
@@ -189,7 +189,7 @@ func main() {
 			updateSetting(port, username, password)
 		}
 	default:
-		fmt.Println("except 'run' or 'v2-ui' or 'setting' subcommands")
+		fmt.Println("Các lệnh con ngoại trừ 'run' hoặc 'v2-ui' hoặc 'setting' ")
 		fmt.Println()
 		runCmd.Usage()
 		fmt.Println()
